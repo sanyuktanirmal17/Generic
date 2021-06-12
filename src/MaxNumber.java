a7c06cb08a58ab9cf16d385d7149ad0f580f82a4
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-12 20:26:02 +0530

    Added: Float value

--- src/MaxNumber.java
+++ src/MaxNumber.java
@@ -1,14 +1,13 @@
-package com;
-
 /**
  * MaximumCheck is an class of public type
- * @author sanyukta Nirmal
+ * @author sanyukta
  *
  */
 public class MaxNumber {
 	
 	/**
-	 * calculating Maximum of Three Numbers using CompareTo method
+	 .*calculating Maximum of Three Numbers using CompareTo method
+	 *returning int max value
 	 */
 	
 		public static int checkMaximum(Integer x, Integer y, Integer z) {
@@ -22,14 +21,31 @@ public class MaxNumber {
 			return max; 
 		}
 		
-		/*
-		 *printing max value
+		/**
+		 * checkMaximum is a method to check the maximum value in float.
+		 */
+		
+		public static Double checkMaximum(Double a, Double b, Double c) {
+			Double max = a;
+			if(b.compareTo(max) > 0) {
+				max = b; 
+			}
+			if(c.compareTo(max) > 0) {
+				max = c; 
+			}
+			return max; 
+		}
+		
+		/**
+		 * main method
 		 */
 		
 		public static void main(String[] args) {
 			
-			int c = checkMaximum(10,23,15);
-			System.out.println("The Maximum value is : " +c);
+			int c = checkMaximum(59,55,32);
+			Double d = checkMaximum(4.5,55.0,67.70);
+			System.out.println("The Maximum int value is : " +c);
+			System.out.println("The Maximum float value is : " +d);
 		}
 
 }
