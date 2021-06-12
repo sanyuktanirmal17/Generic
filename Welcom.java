69677dbcd257fb52cd6dfe869236c0232e55b68f
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-12 20:12:56 +0530

    Added: Welcome

---
+++ Welcom.java
@@ -0,0 +1,2 @@
+#Welcome to Find Max number
+
---
+++ src/MaxNumber.java
@@ -0,0 +1,35 @@
+package com;
+
+/**
+ * MaximumCheck is an class of public type
+ * @author sanyukta
+ *
+ */
+public class MaxNumber {
+	
+	/**
+	 * calculating Maximum of Three Numbers using CompareTo method
+	 */
+	
+		public static int checkMaximum(Integer x, Integer y, Integer z) {
+			Integer max = x;
+			if(y.compareTo(max) > 0) {
+				max = y; 
+			}
+			if(z.compareTo(max) > 0) {
+				max = z; 
+			}
+			return max; 
+		}
+		
+		/*
+		 *printing max value
+		 */
+		
+		public static void main(String[] args) {
+			
+			int c = checkMaximum(10,23,15);
+			System.out.println("The Maximum value is : " +c);
+		}
+
+}
